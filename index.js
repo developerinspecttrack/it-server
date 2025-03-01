54fa18d836f2ad481e72b19a0d079eaf9f219b3f
brien-austin-clayton <brian@cybermindworks.com>
2025-03-01 10:09:22 +0530

    basic server setup

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config()
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started on the port ${process.env.PORT}`)
+})
